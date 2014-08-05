a9e1fe4f71880c15a0c7547c30962d79638821b4
dayAlone <andrey.slider@gmail.com>
2014-08-06 00:13:14 +0600

    Changes

--- bitrix/events/detail.php
+++ bitrix/events/detail.php
@@ -7,6 +7,7 @@ $APPLICATION->IncludeComponent("bitrix:news.detail","event",Array(
     "IBLOCK_TYPE"    => "events",
     "ELEMENT_CODE"  => $_REQUEST["ELEMENT_CODE"],
     "CHECK_DATES"   => "N",
+    "CACHE_TYPE"           => "N",
     "FIELD_CODE"    => Array("ID"),
     "SET_TITLE"     => "N",
     "PROPERTY_CODE" => Array("CITY", "DATE", "DIRECTION","DIRECTION.NAME", "SPEAKERS", "TYPE", "GROUP", "ADDRESS", "SHEDULE", "IMAGE"),
--- bitrix/local/templates/mars_graduates/components/bitrix/news.detail/event/component_epilog.php
+++ bitrix/local/templates/mars_graduates/components/bitrix/news.detail/event/component_epilog.php
@@ -1,16 +1,3 @@
 <?
-	if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-	$prop = &$arResult["PROPS"];
-	global $APPLICATION;
-	$APPLICATION->SetTitle($prop['TYPE'].": «".$arResult["NAME"]."»");
-	$APPLICATION->SetPageProperty('day', $prop['DATE']);
-	$APPLICATION->SetPageProperty('og:image', $prop['DIRECTION']['TITLE_IMAGE']);
-	$APPLICATION->SetPageProperty('description', $arResult["DETAIL_TEXT"]);
-	$this->SetViewTarget('day');
-	      require_once($_SERVER['DOCUMENT_ROOT'].'/include/calendar.php');
-	$this->EndViewTarget();
-	$this->SetViewTarget('header');
-			
-	      ?><link rel="image_src" href="http://<?=$_SERVER['SERVER_NAME'].$prop['DIRECTION']['TITLE_IMAGE']?>" /><?
-	$this->EndViewTarget();
+
 ?>
