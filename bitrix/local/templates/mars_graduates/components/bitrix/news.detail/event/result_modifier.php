6fa131dbf7db1b1cfe80550de0b9044a7174bc1a
dayAlone <andrey.slider@gmail.com>
2014-08-07 18:06:23 +0600

    Changes

--- bitrix/local/templates/mars_graduates/components/bitrix/news.detail/event/result_modifier.php
+++ bitrix/local/templates/mars_graduates/components/bitrix/news.detail/event/result_modifier.php
@@ -4,12 +4,17 @@
 	$prop = &$arResult["PROPS"];
 	foreach ($arResult["PROPERTIES"] as $key => $item) {
 		switch ($item['CODE']) {
+			case 'DATE':
+			case 'GROUP':
+			case 'ADDRESS':
+			case 'SHEDULE':
+				$prop[$item["CODE"]] = $item["VALUE"];
+				break;
 			case 'TYPE':
 			case 'CITY':
 				$res = CIBlockElement::GetByID($item["VALUE"]);
-				if($ar_res = $res->GetNext()) {
+				if($ar_res = $res->GetNext())
 				  $prop[$item["CODE"]] = $ar_res['NAME'];
-				}
 				break;
 			case 'IMAGE':
 				$prop["IMAGE"] = CFile::GetPath($$item);
@@ -19,33 +24,24 @@
 				  $res2 = CIBlockElement::GetProperty($ar_res['IBLOCK_ID'], $ar_res['ID'], "sort", "asc", array("CODE" => "IMAGE"));
     			  $image = $res2->Fetch();
 				  $prop[$item["CODE"]] = array(
-				  	"NAME"  => $ar_res['NAME'],
-				  	"IMAGE" => CFile::GetPath($ar_res['DETAIL_PICTURE']),
-				  	"TITLE_IMAGE"=>CFile::GetPath($image["VALUE"]),
+						"NAME"        => $ar_res['NAME'],
+						"IMAGE"       => CFile::GetPath($ar_res['DETAIL_PICTURE']),
+						"TITLE_IMAGE" => CFile::GetPath($image["VALUE"]),
 				  	);
 				}
-				break;
-			case 'SHEDULE':
-				$prop[$item["CODE"]] = $item["VALUE"];
 				break;
 			case 'SPEAKERS':
 				$prop[$item["CODE"]] = array();
 				foreach ($item["VALUE"] as $value) {
 					$res = CIBlockElement::GetByID($value);
-					if($ar_res = $res->GetNext()) {
+					if($ar_res = $res->GetNext())
 						$prop[$item["CODE"]][] = array(
 							"NAME"  => $ar_res["NAME"],
 							"IMAGE" => CFile::GetPath($ar_res["PREVIEW_PICTURE"]),
 							"TEXT"  => $ar_res["PREVIEW_TEXT"]
 						);
-					}
 				}
 				 
-				break;
-			case 'DATE':
-			case 'GROUP':
-			case 'ADDRESS':
-				$prop[$item["CODE"]] = $item["VALUE"];
 				break;
 		}
 	}
