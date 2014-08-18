0cbe9ade9aafa1ed533d63d8636e34de1ebe1526
dayAlone <andrey.slider@gmail.com>
2014-08-18 20:19:50 +0600

    Side

--- bitrix/local/templates/mars_graduates/components/bitrix/news.list/career_side/template.php
+++ bitrix/local/templates/mars_graduates/components/bitrix/news.list/career_side/template.php
@@ -1,7 +1,9 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 $this->setFrameMode(true);
+if(count($arResult['ITEMS'])>0):
 $open = array();
 foreach ($arResult['ITEMS'] as $key => &$item):
 	$open[] = $item['NAME'];
 endforeach;?>
 <a href="/career/" class="flag">Внимание, открыт <br>набор на <?=implode(" и ", $open)?>!<?=svg('flag')?></a>
+<?endif;?>
